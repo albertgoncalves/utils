5c25291b1ea7e77691a466d40e92579c3caaaca1
albertgoncalves <goncalves.albert@gmail.com>
2018-11-11 15:47:00 -0500

    Better helper.

--- setup_hs.sh
+++ setup_hs.sh
@@ -5,35 +5,22 @@ cat << 'EOF' >> $1.hs
 EOF
 
 cat << 'EOF' >> shell.nix
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc861" }:
+{ pkgs ? import <nixpkgs> {} }:
 
-let
-
-    inherit (nixpkgs) pkgs;
-
-    f = { mkDerivation, base, stdenv }:
-        mkDerivation {
-            pname = "haskell";
-            version = "0";
-            src = ./.;
-            isLibrary = false;
-            isExecutable = true;
-            executableHaskellDepends = [ base
-                                         haskellPackages.random
-                                       ];
-            license = stdenv.lib.licenses.gpl3;
-        };
-
-    haskellPackages = if compiler == "default"
-                          then pkgs.haskellPackages
-                      else
-                          pkgs.haskell.packages.${compiler};
-
-    drv = haskellPackages.callPackage f {};
-
-in
-
-    if pkgs.lib.inNixShell
-        then drv.env
-    else drv
+with pkgs; mkShell {
+    name = "haskell";
+    buildInputs = [ (haskell.packages.ghc861.ghcWithPackages (pkgs: [
+                      pkgs.base
+                    ]))
+                    haskellPackages.hlint
+                    libiconv
+                    fzf
+                  ];
+    shellHook = ''
+        withfzf() { $1 "$(fzf)"; }
+        alias vimfzf="withfzf vim"
+        alias runfzf="withfzf runhaskell"
+        alias hlifzf="withfzf hlint -c=never"
+    '';
+}
 EOF
